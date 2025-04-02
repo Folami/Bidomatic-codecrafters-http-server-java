6d3f73565e03cf4343362c09e4e07207dd511b2d
Folami <folami.bidmus@gmail.com>
2025-04-02 13:24:45 +0100

    [Respond with 200]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,29 @@
+import java.io.OutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+        int port = 4221;
 
-    try {
-      ServerSocket serverSocket = new ServerSocket(4221);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                // Tester restarts program quite often, setting SO_REUSEADDR
+                // ensures that we don't run into 'Address already in use' errors
+                serverSocket.setReuseAddress(true);
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+                OutputStream out = clientSocket.getOutputStream();
+                String response = "HTTP/1.1 200 OK\r\n\r\n";
+                out.write(response.getBytes("UTF-8"));
+                out.flush();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
